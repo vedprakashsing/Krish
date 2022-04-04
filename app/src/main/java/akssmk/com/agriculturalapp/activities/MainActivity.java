6b9614969b1dad74bac82bf502fca09b8ee1fbaf
Sanskar Agrawal <sanskar10100@gmail.com>
2022-04-04 22:46:07 +0530

    Rename app and fix some issues

--- app/src/main/java/akssmk/com/agriculturalapp/activities/MainActivity.java
+++ app/src/main/java/akssmk/com/agriculturalapp/activities/MainActivity.java
@@ -28,29 +28,25 @@ import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
-import akssmk.com.agriculturalapp.modals.MainListItem;
 import akssmk.com.agriculturalapp.R;
 import akssmk.com.agriculturalapp.adapters.MainAdapter;
+import akssmk.com.agriculturalapp.modals.MainListItem;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private final Integer[] imageUrls = {R.raw.crop_production_opt, R.raw.treat, R.raw.shc2, R.drawable.production_main, R.drawable.horticulture_main, R.raw.govp};
+    private final Integer[] hindiTexts = {R.string.crop_production_card_title_hi, R.string.treatment_card_title_hi,
+            R.string.storage_card_title_hi, R.string.survey_card_title_hi,
+            R.string.horticulture_card_title_hi, R.string.policy_card_title_hi};
+    private final Integer[] englishTexts = {R.string.crop_production_card_title_en, R.string.treatment_card_title_en,
+            R.string.storage_card_title_en, R.string.survey_card_title_en,
+            R.string.horticulture_card_title_en, R.string.policy_card_title_en};
+    private final String[] backgroundColors = {"#35e372", "#a4f075", "#ffff4d", "#70dbdb", "#cef63c", "#ff9f80"};
     private ArrayList<MainListItem> list;
     private RecyclerView recyclerView;
     private MainAdapter adapter;
-
-    private Integer[] imageUrls={R.raw.crop_production_opt,R.raw.treat,R.raw.shc2,R.drawable.production_main,R.drawable.horticulture_main,R.raw.govp};
-
-    private Integer[] hindiTexts={R.string.crop_production_card_title_hi,R.string.treatment_card_title_hi,
-            R.string.storage_card_title_hi,R.string.survey_card_title_hi,
-            R.string.horticulture_card_title_hi,R.string.policy_card_title_hi};
-
-    private Integer[] englishTexts={R.string.crop_production_card_title_en,R.string.treatment_card_title_en,
-            R.string.storage_card_title_en,R.string.survey_card_title_en,
-            R.string.horticulture_card_title_en,R.string.policy_card_title_en};
-
-    private String[] backgroundColors={"#35e372","#a4f075","#ffff4d","#70dbdb","#cef63c","#ff9f80"};
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
@@ -60,7 +56,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Intent[] links={
+        Intent[] links = {
                 new Intent(MainActivity.this, CropProductionActivity.class),
                 new Intent(MainActivity.this, SelectProblem.class),
                 new Intent(MainActivity.this, SoilHealthActivity.class),
@@ -70,8 +66,8 @@ public class MainActivity extends AppCompatActivity
         };
 
         list = new ArrayList<>();
-        for(int i=0;i<imageUrls.length;i++){
-            MainListItem item=new MainListItem();
+        for (int i = 0; i < imageUrls.length; i++) {
+            MainListItem item = new MainListItem();
 
             item.setImageUrl(imageUrls[i]);
             item.setHindiText(hindiTexts[i]);
@@ -82,7 +78,7 @@ public class MainActivity extends AppCompatActivity
             list.add(item);
         }
 
-        adapter = new MainAdapter(this,list);
+        adapter = new MainAdapter(this, list);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -119,26 +115,27 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.bazar_rates_nav) {
-            Log.v("###","hello");
+            Log.v("###", "hello");
             startActivity(new Intent(MainActivity.this, Select_State_Bazaar.class));
 
-        }else if(id == R.id.state_agricultural_nav){
-            startActivity(new Intent(MainActivity.this,StateDepartment.class));
+        } else if (id == R.id.state_agricultural_nav) {
+            startActivity(new Intent(MainActivity.this, StateDepartment.class));
 
-        }else if(id == R.id.about_nav){
+        } else if (id == R.id.about_nav) {
 
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setTitle(String.format("%1$s", getString(R.string.about)));
-            builder.setMessage(getResources().getText(R.string.about_text));
+            builder.setMessage("This app was built as the final year project of B.Tech(CSE) by" +
+                    " Ved Prakash Singh, Amaresh Tiwari, Yashwant Singh, Shubham Yadav and Tanya Singh.");
             builder.setPositiveButton("OK", null);
             builder.setIcon(R.mipmap.ic_launcher);
             AlertDialog welcomeAlert = builder.create();
             welcomeAlert.show();
             ((TextView) welcomeAlert.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance());
 
-        }else if(id == R.id.contactus_nav){
+        } else if (id == R.id.contactus_nav) {
             Intent intent = new Intent(Intent.ACTION_SENDTO);
-            String uriText = "mailto:" + Uri.encode("akhileshnithcse@gmail.com") + "?subject=" +
+            String uriText = "mailto:" + Uri.encode("ved94524060@gmail.com") + "?subject=" +
                     Uri.encode("Feedback") + "&body=" + Uri.encode("");
 
             Uri uri = Uri.parse(uriText);
@@ -146,31 +143,31 @@ public class MainActivity extends AppCompatActivity
             startActivity(Intent.createChooser(intent, "Send Email"));
 
 
-        }else if(id == R.id.call_link){
-            if(Build.VERSION.SDK_INT<23){
-                Intent in=new Intent(Intent.ACTION_CALL, Uri.parse("tel:+91" + "18001801551"));
+        } else if (id == R.id.call_link) {
+            if (Build.VERSION.SDK_INT < 23) {
+                Intent in = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+91" + "18001801551"));
 
-                try{
+                try {
                     startActivity(in);
-                }catch (android.content.ActivityNotFoundException ex){
+                } catch (android.content.ActivityNotFoundException ex) {
                     Toast.makeText(getApplicationContext(), "yourActivity is not founded", Toast.LENGTH_SHORT).show();
                 }
 
-            }else{
+            } else {
                 int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
                 int hasWriteContactsPermission = checkSelfPermission(Manifest.permission.CALL_PHONE);
                 if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
-                    requestPermissions(new String[] {Manifest.permission.CALL_PHONE},
+                    requestPermissions(new String[]{Manifest.permission.CALL_PHONE},
                             REQUEST_CODE_ASK_PERMISSIONS);
 
                 }
 
-                Intent in=new Intent(Intent.ACTION_CALL, Uri.parse("tel:+91" + "18001801551"));
+                Intent in = new Intent(Intent.ACTION_CALL, Uri.parse("tel:+91" + "18001801551"));
 
-                try{
+                try {
                     startActivity(in);
-                }catch (Exception ex){
+                } catch (Exception ex) {
                     Toast.makeText(getApplicationContext(), "Permissions required", Toast.LENGTH_SHORT).show();
                 }
 
